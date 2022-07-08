5638143614217ff86205784dac43b0eefb6047ac
A_biggart <103457162+AmberArt@users.noreply.github.com>
2022-07-08 16:45:41 -0600

    Add files via upload

---
+++ src/main/java/com/mlb/mlb_db/MlbDbApplication.java
@@ -0,0 +1,13 @@
+package com.mlb.mlb_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MlbDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MlbDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mlb/mlb_db/MlbDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mlb.mlb_db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MlbDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
